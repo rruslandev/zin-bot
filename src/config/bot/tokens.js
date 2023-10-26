d57912bf9e04d568c119f72a7550744b6c1dc5d7
Ruslan Rasulov <104435530+rruslandev@users.noreply.github.com>
2023-10-26 15:54:17 +0300

    Delete src/config/bot/tokens.js

--- src/config/bot/tokens.js
+++
@@ -1,7 +0,0 @@
-// основной бот(Валерии)
-const BOT_TOKEN = '6507824193:AAFZgc4wDyqSb6hRRMhljhn3caXyXFHeEV0'
-
-// тестовый бот(Руслана)
-// const BOT_TOKEN = '6423348151:AAGSkRVpHtVnpQLOZGn9n8lDc7U_pTQTea8'
-
-module.exports = BOT_TOKEN
