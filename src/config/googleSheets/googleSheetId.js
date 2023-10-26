799ee2d6f63f9ac4c9289fa7c4a1a43cb17879e7
Ruslan Rasulov <104435530+rruslandev@users.noreply.github.com>
2023-10-26 15:54:48 +0300

    Delete src/config/googleSheets/googleSheetId.js

--- src/config/googleSheets/googleSheetId.js
+++
@@ -1,5 +0,0 @@
-// https://docs.google.com/spreadsheets/d/1x8dDlsZYp-De9GkPSBQKZEfojS5EtSJfsLMwfefNBgA/edit?usp=sharing
-
-const googleSheetId = '1x8dDlsZYp-De9GkPSBQKZEfojS5EtSJfsLMwfefNBgA'
-
-module.exports = googleSheetId
